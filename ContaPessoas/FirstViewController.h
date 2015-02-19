0e3c7186a57d3d2117f40a0fa120590ba96fff25
Lucas Leal Mendonça <Luke_Leal@MacBook-Pro-de-Lucas.local>
2015-02-19 14:21:00 -0200

    Mudança de comentário
    Troca de nome no comentário de uma classe

--- ContaPessoas/FirstViewController.h
+++ ContaPessoas/FirstViewController.h
@@ -2,8 +2,8 @@
 //  FirstViewController.h
 //  ContaPessoas
 //
-//  Created by Vinicius Miana on 2/18/15.
-//  Copyright (c) 2015 Vinicius Miana. All rights reserved.
+//  Created by Lucas Lea Mendonça on 2/18/15.
+//  Copyright (c) 2015 Lucas Leal Mendonça. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
